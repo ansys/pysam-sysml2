9d7372c223e65823c919d40546dc5d476cd75120
AdrienGrzecho <AdrienGrzecho@users.noreply.github.com>
2026-03-24 11:11:44 +0000

    DOC: update development documentation 5c7bd1ebf422b67a556623ec8d7c7d020a2958bd

---
+++ version/dev/_static/code/creating-elements-static.py
@@ -0,0 +1,52 @@
+# Copyright (C) 2024 - 2026 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""Creating element static example for PySAM SysML2."""
+
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+
+from ansys.sam.sysml2 import AnsysSysML2APIConnector, SysML2ProjectManager
+from ansys.sam.sysml2.tools import Factory
+
+# Used to disable warnings
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# Create your connector for the SAM server
+ansyssysml2apiconnector = AnsysSysML2APIConnector(
+    server_url="<SAM Server URL>",  # Your SAM server base URL
+    organization_id="<Orga ID>",  # The organization ID
+    token="<Token>",  # Your authorization token
+    use_ssl=False,  # If the server hasn't a valid SSL
+)
+
+project_manager = SysML2ProjectManager(connector=ansyssysml2apiconnector)
+project = project_manager.get_sysml_project("<Bike Project ID>")
+
+bike = project.get_root_package().get("Structure").get("Bike")
+
+factory = Factory(project, ansyssysml2apiconnector)
+
+new_bicycle_frame_length = factory.create_attribute_usage(name="length", owner=bike.get("frame"))
+
+new_bicycle_frame_length.parse_and_set_value("60 [cm]")
+
+print(bike.get("frame").get("length").get_value())
