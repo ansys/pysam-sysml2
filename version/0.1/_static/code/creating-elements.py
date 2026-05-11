643b4e6bd5b3449b507438349d18391f19faf59e
AdrienGrzecho <AdrienGrzecho@users.noreply.github.com>
2026-05-11 08:18:52 +0000

    DOC: update development documentation a896512804dab2b782d127547d1deb616db01988

---
+++ version/0.1/_static/code/creating-elements.py
@@ -0,0 +1,33 @@
+"""Creating element example for PySAM SysML2."""
+
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+
+from ansys.sam.sysml2 import AnsysSysML2APIConnector, SysML2ProjectManager
+from ansys.sam.sysml2.tools import Factory
+
+# Used to disable warnings
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# Create your connector for the SAM server
+ansyssysml2apiconnector = AnsysSysML2APIConnector(
+    server_url="https://127.0.0.1:8443/",  # Your Sam server base URL
+    organization_id="<Orga ID>",  # The organization ID
+    token="<Token>",  # Your authorization token
+    use_ssl=False,  # If the server has a valid SSL
+)
+
+project_manager = SysML2ProjectManager(connector=ansyssysml2apiconnector)
+project = project_manager.get_project("<Bike Project ID>")
+
+bike = project.get_root_package().Structure.Bike
+
+factory = Factory(project, ansyssysml2apiconnector)
+
+new_bicycle_frame_length = factory.create_elements(
+    element_type="AttributeUsage", name="length", owner=bike.frame
+)
+
+new_bicycle_frame_length.parse_and_set_value("60 [cm]")
+
+print(project.get_root_package().Structure.Bike.frame.length.get_value())
